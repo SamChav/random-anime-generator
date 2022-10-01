1e1a42b68ef4284abe76098180a43873ec34a551
SamChav <chavez.samuel1@gmail.com>
2022-09-30 23:21:21 -0700

    cleaned up a lot of stuff, added guessing game with score, reset button

--- app.js
+++ app.js
@@ -9,18 +9,31 @@ const getShowInfoButton = document.querySelector(".getShowInfoButton");
 const haikyuuButton = document.querySelector("#haikyuuButton");
 const onePieceButton = document.querySelector("#onePieceButton");
 const mhaButton = document.querySelector("#mhaButton");
+const guessingGameButton = document.querySelector(".guessingGameButton");
+const guessingGameWrapper = document.querySelector(".guessingGameWrapper");
+const guessInput = document.querySelector(".guessInput");
+const guessSubmit = document.querySelector(".guessSubmit");
+const resetScoreButton = document.querySelector(".resetScoreButton");
 
 let localCharacterImage = [];
 let localCharacterName = [];
 let charId = 0;
 let showNum = 0;
 let numOfChars = 0;
-listingsContainer.style.display = "none";
+let isAiring = "";
+let guessAttempts = 0;
+let score = 0;
 
-haikyuuButton.addEventListener("click", getHaikyuu);
+listingsContainer.style.display = "none";
+guessingGameWrapper.style.display = "none";
+resetScoreButton.style.display = "none";
 
 async function getHaikyuu() {
+  guessingGameWrapper.style.display = "none";
   appContainer.style.backgroundImage = "url('./background.png')";
+  resetScoreButton.style.display = "none";
+  guessingGameButton.innerHTML = "Guessing Game!";
+  guessingGameButton.style.display = "block";
   charImg.src = "./transparentimg.png";
   infoDiv.innerText = null;
   localCharacterImage = [];
@@ -39,10 +52,12 @@ async function getHaikyuu() {
     });
 }
 
-onePieceButton.addEventListener("click", getOnePiece);
-
 async function getOnePiece() {
+  guessingGameWrapper.style.display = "none";
   appContainer.style.backgroundImage = "url('./onepiece.png')";
+  resetScoreButton.style.display = "none";
+  guessingGameButton.innerHTML = "Guessing Game!";
+  guessingGameButton.style.display = "block";
   charImg.src = "./transparentimg.png";
   infoDiv.innerText = null;
   localCharacterImage = [];
@@ -63,7 +78,11 @@ async function getOnePiece() {
 }
 
 async function mha() {
+  guessingGameWrapper.style.display = "none";
   appContainer.style.backgroundImage = "url('./mha.png')";
+  resetScoreButton.style.display = "none";
+  guessingGameButton.innerHTML = "Guessing Game!";
+  guessingGameButton.style.display = "block";
   charImg.src = "./transparentimg.png";
   infoDiv.innerText = null;
   localCharacterImage = [];
@@ -83,11 +102,29 @@ async function mha() {
     });
 }
 
-mhaButton.addEventListener("click", mha);
+getAnimeCharButton.addEventListener("click", () => {
+  guessingGameButton.innerHTML = "Guessing Game!";
+  guessingGameButton.style.display = "block";
+  charImg.style.filter = "blur(0px)";
+  infoDiv.style.display = "flex";
+  resetScoreButton.style.display = "none";
+  guessingGameWrapper.style.display = "none";
+  const min = Math.ceil(0);
+  const max = Math.floor(numOfChars);
+  charId = Math.floor(Math.random() * (max - min) + min);
 
-let isAiring = "";
+  charImg.src = localCharacterImage[charId];
+
+  infoDiv.innerText = `Name: ${localCharacterName[charId]} \n`;
+});
 
 const showInfoFunc = () => {
+  guessingGameWrapper.style.display = "none";
+  resetScoreButton.style.display = "none";
+  charImg.style.filter = "blur(0px)";
+  infoDiv.style.display = "flex";
+  guessingGameButton.innerHTML = "Guessing Game!";
+  guessingGameButton.style.display = "block";
   axios.get(`https://api.jikan.moe/v4/anime/${showNum}`).then((res) => {
     if (res.data.data.airing === false) {
       isAiring = "Nope";
@@ -105,14 +142,91 @@ const showInfoFunc = () => {
   });
 };
 
-getAnimeCharButton.addEventListener("click", () => {
+const openGuessingGame = (e) => {
+  e.preventDefault();
+  guessingGameButton.style.display = "none";
+  resetScoreButton.style.display = "block";
+  guessAttempts = 0;
+  guessingGameWrapper.style.display = "flex";
   const min = Math.ceil(0);
   const max = Math.floor(numOfChars);
   charId = Math.floor(Math.random() * (max - min) + min);
   charImg.src = localCharacterImage[charId];
   infoDiv.innerText = `Name: ${localCharacterName[charId]} \n`;
-});
+  infoDiv.style.display = "none";
+  charImg.style.filter = "blur(18px)";
+  infoDiv.innerText = `Guesses: ${guessAttempts} / 5`;
+  infoDiv.style.display = "flex";
+};
 
-getShowInfoButton.addEventListener("click", () => {
-  showInfoFunc();
-});
+const userGuessLogic = (e) => {
+  e.preventDefault();
+  const userGuess = e.target.form[0].value;
+  guessInput.value = "";
+  if (
+    guessAttempts === 0 &&
+    !localCharacterName[charId].toLowerCase().split(" ").includes(userGuess)
+  ) {
+    charImg.style.filter = "blur(9px)";
+    guessAttempts++;
+    guessInput.value = "";
+    infoDiv.innerText = `Guesses: ${guessAttempts} / 5`;
+  } else if (
+    guessAttempts === 1 &&
+    !localCharacterName[charId].toLowerCase().split(" ").includes(userGuess)
+  ) {
+    charImg.style.filter = "blur(7px)";
+    guessAttempts++;
+    guessInput.value = "";
+    infoDiv.innerText = `Guesses: ${guessAttempts} / 5`;
+  } else if (
+    guessAttempts === 2 &&
+    !localCharacterName[charId].toLowerCase().split(" ").includes(userGuess)
+  ) {
+    charImg.style.filter = "blur(5px)";
+    guessAttempts++;
+    guessInput.value = "";
+    infoDiv.innerText = `Guesses: ${guessAttempts} / 5`;
+  } else if (
+    guessAttempts === 3 &&
+    !localCharacterName[charId].toLowerCase().split(" ").includes(userGuess)
+  ) {
+    charImg.style.filter = "blur(3px)";
+    guessAttempts++;
+    guessInput.value = "";
+    infoDiv.innerText = `Guesses: ${guessAttempts} / 5`;
+  } else if (
+    localCharacterName[charId].toLowerCase().split(" ").includes(userGuess)
+  ) {
+    infoDiv.style.display = "flex";
+    charImg.style.filter = "blur(0px)";
+    guessInput.value = "";
+    score++;
+    infoDiv.innerText = `Name: ${localCharacterName[charId]} \n YOU WIN!!!\n Current Score: ${score}`;
+    guessingGameWrapper.style.display = "none";
+    guessingGameButton.innerHTML = "Play again?";
+    guessingGameButton.style.display = "block";
+  } else {
+    infoDiv.style.display = "flex";
+    charImg.style.filter = "blur(0px)";
+    guessInput.value = "";
+    score--;
+    infoDiv.innerText = `Name: ${localCharacterName[charId]} \n YOU LOST!!! \n Current Score: ${score}`;
+    guessingGameWrapper.style.display = "none";
+    guessingGameButton.innerHTML = "Play again?";
+    guessingGameButton.style.display = "block";
+  }
+};
+
+const resetScore = () => {
+  score = 0;
+  infoDiv.innerText = `Current Score: ${score}`;
+};
+
+mhaButton.addEventListener("click", mha);
+onePieceButton.addEventListener("click", getOnePiece);
+haikyuuButton.addEventListener("click", getHaikyuu);
+getShowInfoButton.addEventListener("click", showInfoFunc);
+guessingGameButton.addEventListener("click", openGuessingGame);
+guessSubmit.addEventListener("click", userGuessLogic);
+resetScoreButton.addEventListener("click", resetScore);
